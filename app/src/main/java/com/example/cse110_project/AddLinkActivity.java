20a1928882a6da896d649a7e0e3f3c1edb050832
jasonli35 <lijaosn@gmail.com>
2022-02-11 21:34:18 -0800

    commit

--- app/src/main/java/com/example/cse110_project/AddLinkActivity.java
+++ app/src/main/java/com/example/cse110_project/AddLinkActivity.java
@@ -2,7 +2,11 @@ package com.example.cse110_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AddLinkActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class AddLinkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_link);
+    }
+
+
+    public void processPhoto(View view) {
+        SharedPreferences preferences = getSharedPreferences("USER_INFO", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        TextView urlView = findViewById(R.id.editTextTextPersonName);
+        Intent intent = new Intent(this, PreviewPhotoActivity.class);
+        startActivity(intent);
+        editor.putString("url", urlView.getText().toString());
+        editor.apply();
     }
 }
