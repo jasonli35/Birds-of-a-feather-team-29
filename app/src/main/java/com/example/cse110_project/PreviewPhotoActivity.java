bc599eb66b42ecd9c8b054f877dd71e362ee14ed
jasonli35 <lijaosn@gmail.com>
2022-02-11 17:34:59 -0800

    need to fix url

---
+++ app/src/main/java/com/example/cse110_project/PreviewPhotoActivity.java
@@ -0,0 +1,14 @@
+package com.example.cse110_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviewPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_photo);
+    }
+}
